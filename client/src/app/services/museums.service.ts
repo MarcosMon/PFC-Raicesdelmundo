c095f111be47a8d03314aa919b257ce981636869
marc monjon <sysmarc13@hotmail.com>
2020-05-13 17:19:13 +0200

    Creado metodo saveMuseum en services, enlazado con el post de parte de servidor

--- client/src/app/services/museums.service.ts
+++ client/src/app/services/museums.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 
+import {Museum} from '../models/museums'
+
 @Injectable({
   providedIn: 'root'
 })
@@ -15,6 +17,9 @@ export class MuseumsService {
     getOneMuseum(id : string){
       return this.http.get(`${this.API_URI}/museums/${id}`)
 
+    }
+    saveMuseum(museum : Museum){
+      return this.http.post(`${this.API_URI}/museums`, museum)
     }
    }
 
