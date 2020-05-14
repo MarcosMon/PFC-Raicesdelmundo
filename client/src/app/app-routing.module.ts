7240e2297219f9e9aa871cc783a69d2faceb04db
marc monjon <sysmarc13@hotmail.com>
2020-05-14 17:06:05 +0200

    museum-list terminado, pintamos los primeros museos con cards y ngfor

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { MuseumListComponent } from '../app/components/museum-list/museum-list.component';
+import { MuseumFormComponent } from '../app/components/museum-form/museum-form.component';
+
 const routes: Routes = [
   {
     path: '',
@@ -11,6 +13,10 @@ const routes: Routes = [
   {
     path: 'museums',
     component: MuseumListComponent
+  },
+  {
+    path: 'museums/add',
+    component: MuseumFormComponent
   }
 ];
 
