990c410873d0c0f3d9c94c0a2fa0a8b821c8b73c
marc monjon <sysmarc13@hotmail.com>
2020-05-14 18:36:21 +0200

    Hecho formulario para guardar mis museos en la base de datos. Implementado metodo saveNewMuseum para ello.

--- client/src/app/components/museum-form/museum-form.component.ts
+++ client/src/app/components/museum-form/museum-form.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostBinding } from '@angular/core';
+import { Museum } from '../../models/museums';
+import {MuseumsService} from '../../services/museums.service';
 
 @Component({
   selector: 'app-museum-form',
@@ -7,9 +9,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MuseumFormComponent implements OnInit {
 
-  constructor() { }
+  @HostBinding('class') classes = 'row';
+
+    museum : Museum = {
+    id: 0,
+    name: '',
+    price: 0,
+    description: '',
+    image: '',
+    created_at: new Date()
+  };
+
+  constructor( private MuseumsService : MuseumsService) { }
 
   ngOnInit() {
   }
+
+  saveNewMuseum(){
+    delete this.museum.created_at;
+    delete this.museum.id;
+    this.MuseumsService.saveMuseum(this.museum).subscribe(
+      res => {
+        console.log(res);
+      },
+      err => console.log(err)
+    )
+  }
+
 
 }
--- client/src/app/models/museums.ts
+++ client/src/app/models/museums.ts
@@ -1,6 +1,7 @@
 export interface Museum{
   id?: number;
   name?: string;
+  price?:number;
   description?: string;
   image? : string;
   created_at? : Date;
