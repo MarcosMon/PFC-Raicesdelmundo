0d6c213d6f31124f9c97c407a79fe7e68609ac7c
marc monjon <sysmarc13@hotmail.com>
2020-05-19 21:10:52 +0200

    Formulario mejorado para la creacion de un nuevo museo y su actualización y añadidos nuevos fields en la tabla museums de la BBDD para que el usuario añada más contenido

--- client/src/app/models/museums.ts
+++ client/src/app/models/museums.ts
@@ -4,6 +4,13 @@ export interface Museum{
   name?: string;
   price?:number;
   description?: string;
+  phone?: string;
+  email?: string;
+  website?: string;
+  openinghours?: string;
+  city? : string;
+  province? : string;
+  postalcode? : string;
   image? : string;
   created_at? : Date;
 }
